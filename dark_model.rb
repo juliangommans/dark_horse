63434f6a356a6eacbccf7ec29bb85a026d1f072c
Geordi Dearns <apprentice@Canada.local>
2015-02-20 11:26:15 +1300

    Added dark_model.rb skeleton, and sample question file 'dark_questions_samples.txt' ready for parsing

---
+++ dark_model.rb
@@ -0,0 +1,24 @@
+require 'dark_controller.rb'
+require 'dark_view.rb'
+#######################################
+class FlashCard
+
+  attr_reader :question, :answer #Makes them avaliable to other classes
+
+  def initialize (question, answer)
+    @question = question
+    @answer = answer
+  end
+
+end
+#######################################
+class FileParsing #getting the questions from dark_questions_samples.txt
+
+  def questions_from_txt_file
+
+
+  end
+
+end
+######################################
+# DRIVER CODE
