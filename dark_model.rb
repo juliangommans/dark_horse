1b3aa37f7197d88bd7c4ca7bb375df824470cafd
Geordi Dearns <apprentice@Canada.local>
2015-02-20 12:24:06 +1300

    Added file parsing and return of current card

--- dark_model.rb
+++ dark_model.rb
@@ -4,17 +4,30 @@ class FlashCard
   attr_reader :question, :answer #Makes them avaliable to other classes
 
   def initialize (question, answer)
-    @question = question
-    @answer = answer
+    @question = question.chomp
+    @answer = answer.chomp
   end
 
 end
 #######################################
 class DarkModel #getting the questions from dark_questions_samples.txt
 
+  def initialize
+    file_parsing
+  end
+
   def file_parsing
+    @flashcards = []
+    questions = File.readlines('dark_questions_samples.txt')
+    until questions empty?
+      question_chunk = questions.slice!(0,3)
+      @flashcards << FlashCard.new(question_chunk[0],question_chunk[1])
+    end
+    @flashcards.shuffle!
+  end
 
-
+  def current_card
+    @flashcards.shift
   end
 
   def scoring_system
